439153c6af56e3eac317b4a68fee8d9cef0a4627
AbhyudayaBora <codeluna99@gmail.com>
2021-06-26 21:08:16 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn project_navigus.wsgi
---
+++ quiz/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.db.models.aggregates import Max
+
+from student.models import Student
+
+
+class Course(models.Model):
+   course_name = models.CharField(max_length=50)
+   question_number = models.PositiveIntegerField()
+   total_marks = models.PositiveIntegerField()
+   def __str__(self):
+        return self.course_name
+
+class Question(models.Model):
+    mark_choices = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+        ('6', '6'),
+        ('7', '7'),
+        ('8', '8'),
+        ('9', '9'),
+        ('10', '10')
+    )
+    course=models.ForeignKey(Course,on_delete=models.CASCADE)
+    marks=models.PositiveIntegerField(choices=mark_choices)
+    question=models.CharField(max_length=600)
+    option1=models.CharField(max_length=200)
+    option2=models.CharField(max_length=200)
+    option3=models.CharField(max_length=200)
+    option4=models.CharField(max_length=200)
+    cat=(('Option1','Option1'),('Option2','Option2'),('Option3','Option3'),('Option4','Option4'))
+    answer=models.CharField(max_length=200,choices=cat)
+
+class Result(models.Model):
+    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    exam = models.ForeignKey(Course,on_delete=models.CASCADE)
+    marks = models.PositiveIntegerField()
+    date = models.DateTimeField(auto_now=True)
+
